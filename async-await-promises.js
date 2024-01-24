b8550b0011d0c58f367a13a89b38309edc8f76db
codhiambo <collins.odhiambo@cellulant.io>
2024-01-24 21:14:14 +0300

    async-await coursework

--- async-await-promises.js
+++ async-await-promises.js
@@ -1,4 +1,26 @@
 const fetch = require('node-fetch');
+// async always returns a promise
+// async & await are not support in IE but only on modern browsers
+
+const getTodos = async () => {
+    const response = await fetch('https://fakestoreapi>>>.com/products/1');
+    if (response.status !== 200) {
+        throw new Error();
+    }
+    const data = await response.json();
+    return data;
+};
+
+console.log(1)
+console.log(2)
+getTodos().then(data => {
+    console.log('getTodos() => ', data);
+}).catch(err => {
+     console.log('Error encountered while processing...',err);
+});
+
+console.log(3)
+console.log(4)
 
 const fetchProducts = async () => {
     return await fetch('https://fakestoreapi.com/products/1')
@@ -8,7 +30,9 @@ fetchProducts()
     .then((res) => res.json())
     .then((data) => {
         console.log(data)
-    });
+    }).catch((err) => {
+    console.log(err);
+});
 
 const respondAfterXSeconds = new Promise((resolve) => {
     let x = 0;
